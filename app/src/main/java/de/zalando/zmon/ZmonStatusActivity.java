3e5ec08736289d39bb3f753a015c4de19eca0663
Ingo Weinzierl <ingo.weinzierl@zalando.de>
2015-08-24 20:23:04 +0200

    Enabled ActionBar and added pause and resume menu items to pause/resume the update of the zmon status.

--- app/src/main/java/de/zalando/zmon/ZmonStatusActivity.java
+++ app/src/main/java/de/zalando/zmon/ZmonStatusActivity.java
@@ -1,10 +1,13 @@
 package de.zalando.zmon;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -13,13 +16,22 @@ import java.util.concurrent.TimeUnit;
 import de.zalando.zmon.client.ZmonStatusService;
 import de.zalando.zmon.client.domain.ZmonStatus;
 
-public class ZmonStatusActivity extends Activity {
+public class ZmonStatusActivity extends AppCompatActivity {
+
+    private static final String EXTRA_IS_STATUS_UPDATER_PAUSED = "extra.is.status.updater.paused";
+    private static final String EXTRA_QUEUE_SIZE = "extra.queue.size";
+    private static final String EXTRA_ACTIVE_WORKERS = "extra.active.workers";
+    private static final String EXTRA_TOTAL_WORKERS = "extra.total.workers";
+
+    private MenuItem pauseItem;
+    private MenuItem resumeItem;
 
     private TextView queueSize;
     private TextView activeWorkers;
     private TextView totalWorkers;
 
     private ScheduledThreadPoolExecutor statusUpdateExecutor = new ScheduledThreadPoolExecutor(5);
+    private boolean isStatusUpdateExecutorPaused = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +41,37 @@ public class ZmonStatusActivity extends Activity {
         queueSize = (TextView) findViewById(R.id.queue_size);
         activeWorkers = (TextView) findViewById(R.id.active_workers);
         totalWorkers = (TextView) findViewById(R.id.total_workers);
+
+        if (savedInstanceState != null) {
+            isStatusUpdateExecutorPaused = savedInstanceState.getBoolean(EXTRA_IS_STATUS_UPDATER_PAUSED, false);
+            queueSize.setText(savedInstanceState.getString(EXTRA_QUEUE_SIZE));
+            activeWorkers.setText(savedInstanceState.getString(EXTRA_ACTIVE_WORKERS));
+            totalWorkers.setText(savedInstanceState.getString(EXTRA_TOTAL_WORKERS));
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        pauseItem = menu.findItem(R.id.pause);
+        resumeItem = menu.findItem(R.id.resume);
+
+        pauseItem.setVisible(!isStatusUpdateExecutorPaused);
+        resumeItem.setVisible(isStatusUpdateExecutorPaused);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_zmonstatus, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        if (statusUpdateExecutor != null) {
-            Log.w("[zmon]", "ScheduledThreadPoolExecutor is alive although he should not be!");
-            statusUpdateExecutor.shutdownNow();
-        }
-
-        Log.d("[zmon]", "Start zmon status updater");
-
-        statusUpdateExecutor = new ScheduledThreadPoolExecutor(1);
-        statusUpdateExecutor.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                updateZmonStatus();
-            }
-        }, 0, 5000, TimeUnit.MILLISECONDS);
+        startStatusUpdateExecutor();
     }
 
     @Override
@@ -61,6 +84,63 @@ public class ZmonStatusActivity extends Activity {
     protected void onStop() {
         super.onStop();
         shutdownStatusUpdateExecutor();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(EXTRA_IS_STATUS_UPDATER_PAUSED, isStatusUpdateExecutorPaused);
+        outState.putString(EXTRA_QUEUE_SIZE, queueSize.getText().toString());
+        outState.putString(EXTRA_ACTIVE_WORKERS, activeWorkers.getText().toString());
+        outState.putString(EXTRA_TOTAL_WORKERS, totalWorkers.getText().toString());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.pause:
+                pauseStatusUpdaterExecutor();
+                return true;
+
+            case R.id.resume:
+                resumeStatusUpdaterExecutor();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void startStatusUpdateExecutor() {
+        if (statusUpdateExecutor != null) {
+            statusUpdateExecutor.shutdownNow();
+        }
+
+        if (!isStatusUpdateExecutorPaused) {
+            Log.d("[zmon]", "Start zmon status updater");
+
+            statusUpdateExecutor = new ScheduledThreadPoolExecutor(1);
+            statusUpdateExecutor.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    updateZmonStatus();
+                }
+            }, 0, 5000, TimeUnit.MILLISECONDS);
+        }
+    }
+
+    private void pauseStatusUpdaterExecutor() {
+        Log.d("[zmon]", "Pause zmon status updater");
+        isStatusUpdateExecutorPaused = true;
+        pauseItem.setVisible(false);
+        resumeItem.setVisible(true);
+        shutdownStatusUpdateExecutor();
+    }
+
+    private void resumeStatusUpdaterExecutor() {
+        Log.d("[zmon]", "Resume zmon status updater");
+        isStatusUpdateExecutorPaused = false;
+        pauseItem.setVisible(true);
+        resumeItem.setVisible(false);
+        startStatusUpdateExecutor();
     }
 
     private void shutdownStatusUpdateExecutor() {
