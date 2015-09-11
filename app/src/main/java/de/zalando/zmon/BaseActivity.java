eb8cd7b98fb54aba4b85aa3bdbc1a7bfe9bc200f
Ingo Weinzierl <ingo.weinzierl@zalando.de>
2015-09-11 10:23:00 +0200

    Added toolbar including menu item to open the navigation drawer to all activities.

--- app/src/main/java/de/zalando/zmon/BaseActivity.java
+++ app/src/main/java/de/zalando/zmon/BaseActivity.java
@@ -3,6 +3,9 @@ package de.zalando.zmon;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.ListView;
 
 import de.zalando.zmon.navigation.NavigationClickListener;
@@ -22,6 +25,20 @@ public abstract class BaseActivity extends AppCompatActivity {
         navigationDrawer = (ListView) findViewById(R.id.navigation_drawer);
         navigationDrawer.setAdapter(new NavigationItemAdapter(this));
         navigationDrawer.setOnItemClickListener(new NavigationClickListener(this));
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_top);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+
+            toolbar.setTitle(R.string.label_dashboard);
+            toolbar.setNavigationIcon(R.drawable.ic_menu_white_36dp);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    drawerLayout.openDrawer(Gravity.START);
+                }
+            });
+        }
     }
 
     protected abstract int getLayoutId();
