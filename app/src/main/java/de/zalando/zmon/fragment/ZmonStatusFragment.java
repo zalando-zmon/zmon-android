bb8cd7b985a52e1176b0da376c92007d5be559e2
Malte Pickhan <malte.pickhan@zalando.de>
2015-12-15 14:33:49 +0100

    Added progressbar to Statusdashboard

--- app/src/main/java/de/zalando/zmon/fragment/ZmonStatusFragment.java
+++ app/src/main/java/de/zalando/zmon/fragment/ZmonStatusFragment.java
@@ -1,14 +1,18 @@
 package de.zalando.zmon.fragment;
 
+import de.zalando.zmon.R;
+import de.zalando.zmon.client.domain.ZmonStatus;
+
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import de.zalando.zmon.R;
-import de.zalando.zmon.client.domain.ZmonStatus;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class ZmonStatusFragment extends Fragment {
 
@@ -19,47 +23,65 @@ public class ZmonStatusFragment extends Fragment {
     private TextView queueSize;
     private TextView activeWorkers;
     private TextView totalWorkers;
+    private ProgressBar progressBar;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+            final Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_zmonstatus, container, false);
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(final View view, final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         queueSize = (TextView) view.findViewById(R.id.queue_size);
         activeWorkers = (TextView) view.findViewById(R.id.active_workers);
         totalWorkers = (TextView) view.findViewById(R.id.total_workers);
+        progressBar = (ProgressBar) view.findViewById(R.id.workers_progress_bar);
 
         if (savedInstanceState != null) {
             queueSize.setText(savedInstanceState.getString(EXTRA_QUEUE_SIZE));
-            activeWorkers.setText(savedInstanceState.getString(EXTRA_ACTIVE_WORKERS));
-            totalWorkers.setText(savedInstanceState.getString(EXTRA_TOTAL_WORKERS));
+
+            float activeWorkersText = Float.valueOf(savedInstanceState.getString(EXTRA_ACTIVE_WORKERS));
+            float totalWorkersText = Float.valueOf(savedInstanceState.getString(EXTRA_TOTAL_WORKERS));
+            activeWorkers.setText(String.valueOf(Math.round(activeWorkersText)));
+            totalWorkers.setText(String.valueOf(Math.round(totalWorkersText)));
+            setWorkerProgressBarProgress(activeWorkersText, totalWorkersText);
         }
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    public void onSaveInstanceState(final Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(EXTRA_QUEUE_SIZE, queueSize.getText().toString());
         outState.putString(EXTRA_ACTIVE_WORKERS, activeWorkers.getText().toString());
         outState.putString(EXTRA_TOTAL_WORKERS, totalWorkers.getText().toString());
     }
 
-    public void update(ZmonStatus status) {
+    public void update(final ZmonStatus status) {
         updateZmonStatusNumbers(status);
         updateZmonStatusColors(status);
     }
 
-    private void updateZmonStatusNumbers(ZmonStatus status) {
-        queueSize.setText(Integer.toString(status.getQueueSize()));
-        activeWorkers.setText(Integer.toString(status.getWorkersActive()));
-        totalWorkers.setText(Integer.toString(status.getWorkersTotal()));
+    private void setWorkerProgressBarProgress(final float active, final float total) {
+
+        int result = Math.round((active / total) * 100);
+
+        progressBar.setProgress(result);
     }
 
-    private void updateZmonStatusColors(ZmonStatus status) {
+    private void updateZmonStatusNumbers(final ZmonStatus status) {
+        queueSize.setText(Integer.toString(status.getQueueSize()));
+
+        float workersActive = status.getWorkersActive();
+        float workersTotal = status.getWorkersTotal();
+        activeWorkers.setText(String.valueOf(workersActive));
+        totalWorkers.setText(String.valueOf(workersTotal));
+        setWorkerProgressBarProgress(workersActive, workersTotal);
+    }
+
+    private void updateZmonStatusColors(final ZmonStatus status) {
         if (status.getQueueSize() > 1000) {
             queueSize.setTextColor(getResources().getColor(R.color.status_warning));
         } else {
