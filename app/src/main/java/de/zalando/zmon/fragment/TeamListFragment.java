a38e070d9ccfc4fda0f42a06fe1a51538982bdb4
Malte Pickhan <malte.pickhan@zalando.de>
2015-12-16 10:37:07 +0100

    Added Searchview after Merge again

--- app/src/main/java/de/zalando/zmon/fragment/TeamListFragment.java
+++ app/src/main/java/de/zalando/zmon/fragment/TeamListFragment.java
@@ -1,19 +1,24 @@
 package de.zalando.zmon.fragment;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
 import java.util.List;
 
 import de.zalando.zmon.R;
 import de.zalando.zmon.adapter.TeamListAdapter;
 import de.zalando.zmon.persistence.Team;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 public class TeamListFragment extends Fragment {
 
@@ -23,23 +28,27 @@ public class TeamListFragment extends Fragment {
 
     private ListView teamList;
 
+    private SearchView teamSearch;
+
     private Callback callback;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+            final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_team_list, container, false);
 
         teamList = (ListView) view.findViewById(R.id.team_list);
+        teamSearch = (SearchView) view.findViewById(R.id.team_search);
         teamList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Team team = (Team) adapterView.getAdapter().getItem(i);
+                @Override
+                public void onItemClick(final AdapterView<?> adapterView, final View view, final int i, final long l) {
+                    Team team = (Team) adapterView.getAdapter().getItem(i);
 
-                if (callback != null) {
-                    callback.onTeamSelected(team);
+                    if (callback != null) {
+                        callback.onTeamSelected(team);
+                    }
                 }
-            }
-        });
+            });
 
         return view;
     }
