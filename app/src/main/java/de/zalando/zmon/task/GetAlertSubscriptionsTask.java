6fda11b4a1d7513193356c7bad238fecf1dc0915
Ingo Weinzierl <ingo.weinzierl@zalando.de>
2015-12-19 20:20:06 +0100

    Reverted icon changes in order to fix app crashes.

---
+++ app/src/main/java/de/zalando/zmon/task/GetAlertSubscriptionsTask.java
@@ -0,0 +1,21 @@
+package de.zalando.zmon.task;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.util.List;
+
+import de.zalando.zmon.client.ServiceFactory;
+
+public class GetAlertSubscriptionsTask extends HttpSafeAsyncTask<Void, Void, List<String>> {
+
+    public GetAlertSubscriptionsTask(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected List<String> callSafe(Void... params) {
+        Log.d("[rest]", "list all active alerts");
+        return ServiceFactory.createZmonService(context).listSubscriptions();
+    }
+}
